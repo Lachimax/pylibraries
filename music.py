957a3db5c5f4172e046c3388b6a3f360a2a930c8
Lachlan Marnoch <lachlan.marnoch@students.mq.edu.au>
2020-09-03 22:13:23 +1000

    Implemented version of SongTree that uses dictionaries

--- music.py
+++ music.py
@@ -4,8 +4,9 @@ import taglib as tl
 
 def check_trailing_slash(path):
     path = str(path)
-    if path[-1] != '\\':
-        path += '\\'
+    if path != "":
+        if path[-1] != '\\':
+            path += '\\'
     return path
 
 
@@ -17,6 +18,138 @@ def get_filetype(path):
 
 
 song_filetypes = ['mp3', 'm4a', 'm4p', 'MP3', 'aif', 'm4v', 'Mp3', 'wav', 'mpg']
+
+
+class SongDictTree:
+    def __init__(self, path: str, type: str = 'Google Play Music'):
+        """
+
+        :param path: This should be the high-level directory in which the folders named after artists are contained.
+        """
+        self.path = check_trailing_slash(path)
+        self.artists = {}
+        self.populate()
+
+    def populate(self):
+        print('Building tree...')
+        for artist_dir in filter(lambda n: os.path.isdir(self.path + n), os.listdir(self.path)):
+            artist_dir = self.path + check_trailing_slash(artist_dir)
+            for album_dir in filter(lambda n: os.path.isdir(artist_dir + n), os.listdir(artist_dir)):
+                album_dir = artist_dir + check_trailing_slash(album_dir)
+                for song in filter(lambda f: get_filetype(f) in song_filetypes, os.listdir(album_dir)):
+                    self.add_song(path=album_dir + song)
+        print('Done.')
+
+    def add_song(self, path):
+        song = Song(path)
+        artist = song.tags['ARTIST'][0]
+        album = song.tags['ALBUM'][0]
+        title = song.tags['TITLE'][0]
+        if self.artists.get(artist) is None:
+            self.artists[artist] = ArtistDictTree(title=artist)
+        if self.artists[artist].get(album) is None:
+            self.artists[artist][album] = AlbumDictTree(title=album)
+        if self.artists[artist][album].get(title) is None:
+            self.artists[artist][album][title] = song
+        else:
+            print("Attempted to add duplicate song,", title, ';', album, ';', artist)
+
+    def __getitem__(self, item):
+        return self.artists[item]
+
+    def __setitem__(self, key, value):
+        self.artists[key] = value
+
+    def show_artists(self):
+        for artist in self.artists:
+            print(artist)
+
+    def show_albums(self):
+        for artist in self.artists:
+            print(artist)
+            self[artist].show_albums(pad=1)
+
+    def show_songs(self):
+        for artist in self.artists:
+            print(artist)
+            self[artist].show_songs(pad=1)
+
+    def count_songs(self):
+        num = 0
+        for artist in self.artists:
+            num += self[artist].count_songs()
+        return num
+
+class ArtistDictTree:
+    def __init__(self, title: str, path: str = ""):
+        """
+        :param title:
+        :param path: The directory containing the albums by a certain artist.
+        """
+        self.title = str(title)
+        self.path = check_trailing_slash(path)
+        self.albums = {}
+
+    def __getitem__(self, item):
+        return self.albums[item]
+
+    def __setitem__(self, key, value):
+        self.albums[key] = value
+
+    def get(self, item):
+        return self.albums.get(item)
+
+    def show_albums(self, pad: int = 0):
+        padding = ""
+        for i in range(pad):
+            padding += "\t"
+        for album in self.albums:
+            print(padding + album)
+
+    def show_songs(self, pad: int = 0):
+        padding = ""
+        for i in range(pad):
+            padding += "\t"
+        for album in self.albums:
+            print(padding + album)
+            self[album].show_songs(pad=pad + 1)
+
+    def count_songs(self):
+        num = 0
+        for album in self.albums:
+            num += self[album].count_songs()
+        return num
+
+
+class AlbumDictTree:
+    def __init__(self, title: str, path: str = ""):
+        """
+
+        :param title:
+        :param path: The directory containing the songs of an album.
+        """
+        self.title = str(title)
+        self.path = check_trailing_slash(path)
+        self.songs = {}
+
+    def __getitem__(self, item):
+        return self.songs[item]
+
+    def __setitem__(self, key, value):
+        self.songs[key] = value
+
+    def get(self, item):
+        return self.songs.get(item)
+
+    def show_songs(self, pad: int = 0):
+        padding = ""
+        for i in range(pad):
+            padding += "\t"
+        for song in self.songs:
+            print(padding + song)
+
+    def count_songs(self):
+        return len(self.songs)
 
 
 class SongTree:
@@ -164,8 +297,11 @@ class Album:
 
 
 class Song:
-    def __init__(self, title: str, path: str):
+    def __init__(self, path: str, title: str = ""):
         self.title = str(title)
         self.path = path
         self.filetype = get_filetype(self.path)
-        #self.file = tl.File(self.path)
+        # print("Loading", self.path)
+        self.file = tl.File(self.path)
+        # print("Done.")
+        self.tags = self.file.tags
