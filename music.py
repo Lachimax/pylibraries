2ee532f1e697eb80bba811675fc2057d38b5bfb4
Lachlan Marnoch <lachlan.marnoch@students.mq.edu.au>
2020-10-06 21:28:28 +1100

    Moved some general functions to utils

--- music.py
+++ music.py
@@ -1,49 +1,8 @@
 import os
 import taglib as tl
 import csv
-from sys import platform
 import shutil
-
-
-def clear_empty_paths(path):
-    for next_path in filter(lambda d: os.path.isdir(path + "\\" + d), os.listdir(path)):
-        print(next_path)
-        next_path = path + "\\" + next_path
-        clear_empty_paths(next_path)
-
-    if os.path.isdir(path):
-        if len(os.listdir(path)) == 0:
-            print("Attempting removal of", path)
-            os.removedirs(path)
-
-def sanitise_path(path):
-    path = path.replace(";", "_").replace("|", "_").replace(",", "_").replace('"', "_").replace("'", "_")
-    path = path.replace(":", "_").replace("*", "_").replace(".", "_").replace("/", "_").replace("\\", "")
-    path = path.replace("^", "_").replace("<", "(").replace(">", ")").replace("?", "_")
-    while path[-1] == " ":
-        path = path[:-1]
-    return path
-
-
-def check_trailing_slash(path):
-    path = str(path)
-    if platform == 'linux':
-        char = '/'
-    else:
-        char = '\\'
-    if path != "":
-        if path[-1] != char:
-            path += char
-    return path
-
-
-def get_filetype(path):
-    pos = -1
-    while abs(pos) <= len(path):
-        if path[pos] == '.':
-            return path[pos + 1:]
-        pos = pos - 1
-    return 'directory'
+from utils import clear_empty_paths, sanitise_path, check_trailing_slash, get_filetype
 
 
 def get_filename(path):
@@ -80,6 +39,8 @@ class SongDictTree:
             self.path = check_trailing_slash(path)
             if populate:
                 self.populate(delete_duplicate=delete_duplicate, sort_files=sort_files, recurse=recurse)
+                if sort_files:
+                    clear_empty_paths(path=path)
         else:
             self.path = None
 
